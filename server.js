7d1fa53c922278551a27ccdc34322d2ff4fd391a
Dylan Caldwell <dylancaldwell@Dylans-iMac.lan>
2021-09-30 18:10:58 -0400

    trying mediaid rather than contentid

--- server.js
+++ server.js
@@ -26,38 +26,58 @@ const scrapeDataFrom = async (scrapeID) => {
     const page = await browser.newPage();
 
     // Login to ADMIN ACCOUNT
-    await page.goto('https://www.edpuzzle.com');
+    // await page.goto('https://www.edpuzzle.com');
 
   
-    await page.click('.hidden-md.hidden-lg.fQ1t4fqKVH._3Zz5F9H2IZ');
+    // await page.click('.hidden-md.hidden-lg.fQ1t4fqKVH._3Zz5F9H2IZ');
 
-    await page.click('._2w1-BNW32w._33wl7jxuFe._2oebyc6eFr._1NXfAQegcd._2TT-CHq6hp');
+    // await page.click('._2w1-BNW32w._33wl7jxuFe._2oebyc6eFr._1NXfAQegcd._2TT-CHq6hp');
 
-    await page.click('._33wl7jxuFe._1hmVhe2bx_._1NXfAQegcd._3Fkf7tortz');
+    // await page.click('._33wl7jxuFe._1hmVhe2bx_._1NXfAQegcd._3Fkf7tortz');
 
-    await page.type('#username', 'misunderstoodkillah@gmail.com');
-    await page.type('#password', 'edpuzzlehack');
-    await page.click('button[type=submit]');
+    // await page.type('#username', 'misunderstoodkillah@gmail.com');
+    // await page.type('#password', 'edpuzzlehack');
+    // await page.click('button[type=submit]');
 
-    // wait for page to load
-    await page.waitForNavigation( { waitUntil: 'networkidle2' } );
+    // // wait for page to load
+    // await page.waitForNavigation( { waitUntil: 'networkidle2' } );
 
-    // GET DATA FROM SCRAPE
+    // // GET DATA FROM SCRAPE
 
-    // FIRST FIND CONTENT ID
-    await page.goto('https://edpuzzle.com/api/v3/assignments/' + scrapeID, {
-        waitUntil: 'networkidle2'
-    });
+    // // FIRST FIND CONTENT ID
+    // await page.goto('https://edpuzzle.com/api/v3/assignments/' + scrapeID, {
+    //     waitUntil: 'networkidle2'
+    // });
 
-    const html = await page.evaluate(() => {
-        return document.body.children[0].innerHTML;
-    });
-    const body = JSON.parse(html);
-    if (body.error) return console.error(body.error);
-    const contentID = body.teacherAssignments[0].contentId;
+    // const html = await page.evaluate(() => {
+    //     return document.body.children[0].innerHTML;
+    // });
+    // const body = JSON.parse(html);
+    // if (body.error) {
+    //   console.error(body.error);
+    //   return body;
+    // }
+    // const contentID = body.teacherAssignments[0].contentId;
 
     // GET SCRAPE DATA WITH NEW ID
-    await page.goto('https://edpuzzle.com/api/v3/media/' + contentID, {
+    // await page.goto('https://edpuzzle.com/api/v3/media/' + contentID, {
+    //     waitUntil: 'networkidle2',
+    // });
+
+    // const newhtml = await page.evaluate(() => {
+    //     return document.body.children[0].innerHTML;
+    // });
+    // const data = JSON.parse(newhtml);
+    
+    
+    // await browser.close();
+
+    // return {
+    //     questions: data.questions.sort((a, b) => a.time - b.time),
+    //     title: data.title,
+    //     img: data.thumbnailURL,
+    // }
+    await page.goto('https://edpuzzle.com/api/v3/media/' + mediaID, {
         waitUntil: 'networkidle2',
     });
 
@@ -80,6 +100,12 @@ const scrapeDataFrom = async (scrapeID) => {
 app.post('/edpuzzle/getdata', async (req, res) => {
     try {
         const data = await scrapeDataFrom(req.body.id);
+        if (data.error) {
+          res.json({
+            success: false,
+            data,
+          })
+        }
         res.json({
             success: true,
             data,
@@ -212,7 +238,11 @@ app.post('/kahoot/joingame', async (req, res) => {
     }
   });
 
+  app.get('/test', async (req, res) => {
+    let test = await scrapeDataFrom('61491a466126df4124869b88');
+    res.send(test);
+  })
+
   app.listen(process.env.PORT || 3000, () => {
       console.log('Listening...')
   });
-  
